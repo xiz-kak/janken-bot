45d2935a79953d61c92a9a80fcde9e46065faa5b
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2021-01-06 14:03:39 +0900

    customize ExpressReceiver

--- src/initializers/bolt.ts
+++ src/initializers/bolt.ts
@@ -1,12 +1,18 @@
-const { App } = require(`@slack/bolt`)
+const { App, ExpressReceiver } = require(`@slack/bolt`)
 const { LogLevel } = require(`@slack/logger`)
+
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: `/slack/events`
+})
 
 const logLevel = process.env.SLACK_LOG_LEVEL || LogLevel.INFO
 
 const config = {
   logLevel,
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver
 }
 
 export const app = new App(config)
